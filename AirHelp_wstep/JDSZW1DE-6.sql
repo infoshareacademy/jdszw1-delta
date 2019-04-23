2ed2f3aa2f7076e1d630865de0c2044571a64f83
Kamil Berlinski <38016833+KamilBerlinski@users.noreply.github.com>
2019-04-23 23:01:48 +0200

    Update JDSZW1DE-6.sql

--- AirHelp_wstep/JDSZW1DE-6.sql
+++ AirHelp_wstep/JDSZW1DE-6.sql
@@ -1,6 +1,15 @@
-select me.id as email_id, mk.typ_kampanii as typ_kampanii, ml.id as "id_leadow"
+select me.id as email_id, mk.typ_kampanii as typ_kampanii, ml.id as "id_leadu", w.id as id_wniosku
 from m_email as me
     left join m_kampanie mk on mk.id = me.id_kampanii
-    left join m_lead_kampania mlk on mk.id = mlk.id_kampania
-    left join m_lead ml on mlk.id_lead = ml.id_wniosku
-where ml.id is not null
+    inner join m_lead_kampania mlk on mk.id = mlk.id_kampania
+    inner join m_lead ml on mlk.id_lead = ml.id_wniosku
+    inner join wnioski w on ml.id_wniosku = w.id
+
+select count(me.id) as email_count, ml.id as "id_leadu", w.id as id_wniosku
+from m_email as me
+    left join m_kampanie mk on mk.id = me.id_kampanii
+    inner join m_lead_kampania mlk on mk.id = mlk.id_kampania
+    inner join m_lead ml on mlk.id_lead = ml.id_wniosku
+    inner join wnioski w on ml.id_wniosku = w.id
+group by ml.id, w.id
+order by id_leadu asc
