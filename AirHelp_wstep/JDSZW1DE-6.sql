ab627c53ef177c2e1ea4258b63cb06a0506f6f85
Kamil Berlinski <38016833+KamilBerlinski@users.noreply.github.com>
2019-04-23 23:47:15 +0200

    Update JDSZW1DE-6.sql

--- AirHelp_wstep/JDSZW1DE-6.sql
+++ AirHelp_wstep/JDSZW1DE-6.sql
@@ -8,6 +8,14 @@ from m_email as me
 group by ml.id, w.id
 order by count(me.id) desc;
 
+-- ile kampanii przypadło na jednego leada
+select ml.id as lead_id, count(mk.id) as liczba_kampani
+from m_lead ml
+    left join m_lead_kampania mlk on ml.id = mlk.id_lead
+    left join m_kampanie mk on mlk.id_kampania = mk.id
+group by ml.id
+having count(mk.id) <> 1
+
 -- ile leadow przypadlo na jeden wniosek, tu jest relacja 1 do 1
 select count(m_lead.id), w.id
 from m_lead
