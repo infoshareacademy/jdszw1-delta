daf22adaf1f476aa1374d5bbe6b14c789bbf3a2e
Kamil Berlinski <38016833+KamilBerlinski@users.noreply.github.com>
2019-04-23 23:19:33 +0200

    Update JDSZW1DE-6.sql
    na jednego leada przypada kilka maili (i jest sporo takich samych sum na milach - pierwsza kolumna, i za bardzo nie wiem z czego to może wynikać, bo jest łaczenie po kluczach a zmiana z inner joina na left itp nie robi różnicy), natomiast jeden lead przypada na jeden wniosek, także można ewentualnie zobaczyć które leady były najbardziej pracochłonne i to połączyć już wnioskami i wypłatą świadczenia

--- AirHelp_wstep/JDSZW1DE-6.sql
+++ AirHelp_wstep/JDSZW1DE-6.sql
@@ -1,15 +1,15 @@
-select me.id as email_id, mk.typ_kampanii as typ_kampanii, ml.id as "id_leadu", w.id as id_wniosku
-from m_email as me
-    left join m_kampanie mk on mk.id = me.id_kampanii
-    inner join m_lead_kampania mlk on mk.id = mlk.id_kampania
-    inner join m_lead ml on mlk.id_lead = ml.id_wniosku
-    inner join wnioski w on ml.id_wniosku = w.id
-
+-- ile maili przypadło na leada
 select count(me.id) as email_count, ml.id as "id_leadu", w.id as id_wniosku
 from m_email as me
-    left join m_kampanie mk on mk.id = me.id_kampanii
+    inner join m_kampanie mk on mk.id = me.id_kampanii
     inner join m_lead_kampania mlk on mk.id = mlk.id_kampania
     inner join m_lead ml on mlk.id_lead = ml.id_wniosku
     inner join wnioski w on ml.id_wniosku = w.id
 group by ml.id, w.id
-order by id_leadu asc
+order by count(me.id) desc;
+
+-- ile leadow przypadlo na jeden wniosek, tu jest relacja 1 do 1
+select count(m_lead.id), w.id
+from m_lead
+    left join wnioski w on m_lead.id_wniosku = w.id
+group by w.id
